8c9eaca84a329a484cf0c6e8399766596deb3224
Aki Helin <aki.helin@iki.fi>
2021-01-12 23:30:58 +0200

    updated fasl-buid.scm to work with new init.fasl

--- bin/fasl-build.scm
+++ bin/fasl-build.scm
@@ -1,15 +1,15 @@
 
 ;; Only if core update is necessary
-,load "owl/core.scm" ;; temporarily for new core
-(import (owl core))
+;,load "owl/core.scm" ;; temporarily for new core
+;(import (owl core))
 
-(import (owl sys))
+(import (prefix (owl sys) sys-))
 (import (owl metric))
 
 ;; info -> fasl-size, initial-fasl-size, boot-ms, initial-boot-ms
 
 (define (add-info info ms bytes)
-   (-> info
+   (pipe info
       ;(put 'boot-ms ms)
       (put 'fasl-size bytes)
       (put 'round (+ 1 (get info 'round 0)))
@@ -30,28 +30,29 @@
          (print " - time " (format-time oms) " (first boot)")
          )))
 
+
 (define (fixed-point args info)
    (lets
       ((start (time-ns))
-       (res (system args))
+       (res (sys-system args))
        (elapsed (- (time-ns) start))
        (fasl-size (file-size "fasl/bootp.fasl"))
        (info (add-info info elapsed fasl-size)))
       (report-deltas info)
-      (if (system '("cmp" "-s" "fasl/boot.fasl" "fasl/bootp.fasl"))
+      (if (sys-system '("cmp" "-s" "fasl/boot.fasl" "fasl/bootp.fasl"))
          (begin
             (print "Fasl build complete. Selfcompilation fixed point reached.")
-            (rename "fasl/bootp.fasl" "fasl/ol.fasl"))
+            (sys-rename "fasl/bootp.fasl" "fasl/ol.fasl"))
          (and
             ; check that the compiling image passes tests
-            (system '("sh" "tests/run" "all" "bin/vm" "fasl/bootp.fasl"))
-            (rename "fasl/bootp.fasl" "fasl/boot.fasl")
+            (sys-system '("sh" "tests/run" "all" "bin/vm" "fasl/bootp.fasl"))
+            (sys-rename "fasl/bootp.fasl" "fasl/boot.fasl")
             (fixed-point args info)))))
 
 (λ (args)
    (print)
    (print "Building compiler fixed point to fasl/ol.fasl.")
-   (if (not (file? "fasl/boot.fasl"))
+   (if (not (sys-file? "fasl/boot.fasl"))
       (begin
          ;; use supplied init.fasl from repository as starting point
          (print "Using init.fasl as the initial step.")
@@ -60,5 +61,5 @@
       (if
          (if (string=? (car args) "-f")
             (fixed-point (cdr args) (put empty 'initial-fasl-size (file-size "fasl/boot.fasl")))
-            (system args))
+            (sys-system args))
          0 1)))
